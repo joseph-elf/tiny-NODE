cf19493637530c413d2d67829227672c5c332095
Joseph El-Forzli <61538081+joseph-elf@users.noreply.github.com>
2026-03-17 23:34:18 +0100

    Update update_env.sh

--- update_env.sh
+++ update_env.sh
@@ -2,7 +2,6 @@
 
 # Get your current local IP
 IP=$(ipconfig getifaddr en0)
-IP=89.167.122.101
 # Write the NEXT_PUBLIC_API_URL to .env.local
 echo "NEXT_PUBLIC_API_URL=https://$IP" > .env.local #:8000" > .env.local
 
